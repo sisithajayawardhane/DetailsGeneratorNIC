981891c3898439723bd7b1f0965418e76921ba7c
sisithajayawardhane <sisithajayawardhane@gmail.com>
2023-01-27 23:13:57 +0530

    connected to mongoDb

--- backend/index.js
+++ backend/index.js
@@ -1,16 +1,36 @@
 const express = require("express");
-const mysql = require('mysql');
+const cors = require("cors");
 
 const app = express();
+
 app.use(express.json());
+app.use(cors());
 
 const db = mysql.createConnection({
     host: "localhost",
-    user: "root",
-    password: "",
-    database: "userDB"
+    user: "root1",
+    password: "1234",
+    database: "userDB",
+});
+
+
+
+//register route
+app.post("/register", (req,res) => {
+
+    const username = req.body.username;
+    const password = req.body.password;
+    
+    
+    db.query(
+        "INSERT INTO user (username, password) VALUES (?,?)",
+        [username,password],
+        (err,result) => {
+            alert(err) ;
+        }
+    );
 });
 
 app.listen(3001, ()=>{
     console.log("running server");
-})
+});
--- frontend/src/Register.js
+++ frontend/src/Register.js
@@ -1,16 +1,33 @@
-import React from "react";
+import React, {useState} from "react"; 
 import './App.css';
+import Axios from 'axios';
 
 export default function Register(){
+
+    const [usernameReg,setUsernameReg] = useState("");
+    const [passwordReg,setPasswordReg] = useState("");
+
+    const register = () => {
+        Axios.post("http//localhost:3001/register",
+        {   username:usernameReg,
+            password:passwordReg
+        }).then((response)=>{
+            console.log(response);
+        });
+    };
     return(
         <div>
             <form >
                 <h1>Registration</h1>
                 <label>Username:</label>
-                <input type="text" placeholder="Username"></input><br/>
+                <input type="text" placeholder="Username" onChange={(e)=>{
+                    setUsernameReg(e.target.value);
+                }}></input><br/>
                 <label>Password:</label>
-                <input type="text" placeholder="Password"></input><br/>
-                <button>Register</button>
+                <input type="password" placeholder="Password" onChange={(e)=>{
+                    setPasswordReg(e.target.value);
+                }}></input><br/>
+                <button onClick={register}>Register</button>
                 
             </form>
             
