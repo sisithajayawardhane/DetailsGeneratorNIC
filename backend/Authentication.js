f2c9791dc2e458f53c4b9f60cf7d54d12a968847
sisithajayawardhane <sisithajayawardhane@gmail.com>
2023-01-28 00:02:59 +0530

    register user route created

---
+++ backend/Authentication.js
@@ -0,0 +1,23 @@
+const router = require("express").Router();
+let User = require("../User.js");
+
+//Register user route
+
+router.route("/add").post((req,res)=>{
+    const username = req.body.username;
+    const password = req.body.password;
+    
+
+    const newUser = new User({
+        username,
+        password
+    });
+    
+    newUser.save().then(()=>{
+        res.json("User added");
+    }).catch((err)=>{
+        console.log(err);
+    });
+});
+
+module.exports = router;
--- backend/index.js
+++ backend/index.js
@@ -1,36 +1,38 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
 const cors = require("cors");
-
+const dotenv = require("dotenv");
 const app = express();
+dotenv.config();
 
-app.use(express.json());
+const PORT = process.env.PORT || 8070;
+
 app.use(cors());
+app.use(bodyParser.json());
 
-const db = mysql.createConnection({
-    host: "localhost",
-    user: "root1",
-    password: "1234",
-    database: "userDB",
+const URL = process.env.MONGODB_URL;
+
+mongoose.connect(URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+mongoose.set('strictQuery', false);
+
+const connection = mongoose.connection;
+connection.once("open", () => {
+    console.log("MongoDB Connection Success!");
+});
+
+const userRouter = require("./Authentication.js"); 
+app.use("/user", userRouter);
+
+
+app.listen(PORT, () => {
+    console.log(`Server is up and running on port number: ${PORT}`);
 });
 
 
 
-//register route
-app.post("/register", (req,res) => {
 
-    const username = req.body.username;
-    const password = req.body.password;
-    
-    
-    db.query(
-        "INSERT INTO user (username, password) VALUES (?,?)",
-        [username,password],
-        (err,result) => {
-            alert(err) ;
-        }
-    );
-});
 
-app.listen(3001, ()=>{
-    console.log("running server");
-});
